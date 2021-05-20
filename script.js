49836f13d21eeca9872ddd39c05e666a7aad2339
unknown <olena.denysova88@gmail.com>
2021-05-20 13:23:57 +0200

    fixed bug in isvalid

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function showSuccess(input) {
 }
 
 //email validation
-function isValidEmail() {
+function isValidEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLocaleLowerCase());
 }
